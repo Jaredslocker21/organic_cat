4d751faa73bbbfa1149fc62d7547871c33e75f60
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-05 22:42:17 +0000

    working out index.html bug and css bug

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
-
-from django.contrib import admin
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home')
 ]
