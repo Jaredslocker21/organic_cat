6f4e00035f77972a9bcbb6e70476ebb31b3dc916
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-06 00:06:46 +0000

    import dj_database and resized bg image

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
