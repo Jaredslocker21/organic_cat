b8a621da5c4eb57363da44ade828268140028b2a
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-09 10:52:07 +0000

    finished product size logic

--- cart/views.py
+++ cart/views.py
@@ -4,22 +4,34 @@ from django.shortcuts import render, redirect
 
 
 def view_cart(request):
-    """ A view to return the cart page """
+    """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
 
 
 def add_to_cart(request, item_id):
-    """ Add quantity to cart of that specified product"""
+    """ Add a quantity of the specified product to the shopping cart """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
-    
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-    else:    
-        cart[item_id] = quantity
+
+    if size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['items_by_size'].keys():
+                cart[item_id]['items_by_size'][size] += quantity
+            else:
+                cart[item_id]['items_by_size'][size] = quantity
+        else:
+            cart[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
