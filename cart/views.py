23c2811ad9ade55629f5b49e8bdb5d46235e47c5
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-09 14:54:59 +0000

    add adjust cart logic

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -35,3 +35,38 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    cart = request.session.get('cart', {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+            messages.success(
+                request, f'Updated size {size.upper()} {product.name} quantity to {cart[item_id]["items_by_size"][size]}')
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(
+                request, f'Removed size {size.upper()} {product.name} from your bag')
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(
+                request, f'Updated {product.name} quantity to {cart[item_id]}')
+        else:
+            cart.pop(item_id)
+            messages.success(
+                request, f'Removed {product.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
