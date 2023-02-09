cdc6c6cbb476e5ad02d18866895b1c0750d7eeb5
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-09 15:19:56 +0000

    add checkout url path and buttons template

--- cart/views.py
+++ cart/views.py
@@ -71,6 +71,7 @@ def adjust_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
+
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
