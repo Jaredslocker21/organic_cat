2c04d145a0d41e35c01cc01096e1187938917546
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-10 12:14:56 +0000

    add checkout cart request and orderform to checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
 # Create your views here.
 
 
 def checkout(request):
     """ A view to return the index page """
+    cart = request.session.get('cart')
+    if not cart:
+        message.error(request, 'There is nothing in you cart.')
+        return redirect(reverse('products'))
+
+        order_form = OrderForm()
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            }
 
     return render(request, 'checkout/checkout.html')
