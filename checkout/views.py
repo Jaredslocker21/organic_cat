833745c018e792736d04c2f5cf1f91710fcc767b
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-10 10:47:06 +0000

    add checkout model/view/url add checkout html file. add css inner carousel

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def checkout(request):
+    """ A view to return the index page """
+
+    return render(request, 'checkout/checkout.html')
