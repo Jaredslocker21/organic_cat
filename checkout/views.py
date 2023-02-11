80037af9b3441562c6848abaf3abc638e8bd6876
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-11 17:46:54 +0000

    add inventory comment valid order form items views.py

--- checkout/views.py
+++ checkout/views.py
@@ -30,11 +30,18 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
+
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
+                    product.inventory = product.inventory - quantity
+                    product.save()
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
