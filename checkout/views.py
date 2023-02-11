8f28538899c8f2572d2098464429198caf1fdd82
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-11 08:18:30 +0000

    add stripe elements js file and fx,css,checkout js block

--- checkout/views.py
+++ checkout/views.py
@@ -10,10 +10,10 @@ def checkout(request):
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('products'))
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
