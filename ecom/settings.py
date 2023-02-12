800aeaef20b9d7f0905bcab2e1f7d86199b5f31b
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-12 15:42:06 +0000

    add profile veiws

--- ecom/settings.py
+++ ecom/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
     'crispy_forms',
+    'profiles'
 ]
 
 MIDDLEWARE = [
--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_countries.fields import CountryField
+
 
 class UserProfile(models.Model):
     """
