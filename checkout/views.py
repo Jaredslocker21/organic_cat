a1c585a65027121ba3af49c842a386eb5cf4c2a6
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-12 16:02:45 +0000

    adduserprofile to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from products.models import Product
-from cart.contexts import cart_contents
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
+from bag.contexts import bag_contents
 
 import stripe
 import json
