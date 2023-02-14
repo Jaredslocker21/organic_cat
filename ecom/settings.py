58c598d99ca4790a956007b5d1da448ae7304772
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-14 15:53:02 +0000

    test media files deployed

--- ecom/settings.py
+++ ecom/settings.py
@@ -21,7 +21,7 @@ if os.path.exists("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productions
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
