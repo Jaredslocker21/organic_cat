24c39997a4280fd608710d4265735122d482f3eb
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-16 14:56:17 +0000

    add urls.py ecom and reviews add edit add review templates

---
+++ reviews/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add/<int:product_id>/', views.add_review, name='add_review'),
+    path('edit/<int:review_id>/', views.edit_review, name='edit_review'),
+    path('delete/<int:review_id>/', views.delete_review, name='delete_review'),
+]
