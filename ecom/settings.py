62b872c3a9490067fc43b83bb1910bdeb8a88ada
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-19 14:52:25 +0000

    fixed error html

--- ecom/settings.py
+++ ecom/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for productions
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
