282eb36d661772c3e2790e07e8fac63a4053a37c
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-20 07:10:14 +0000

    moved inventory higher in views.py before lineitem save

--- checkout/views.py
+++ checkout/views.py
@@ -57,17 +57,19 @@ def checkout(request):
             order.stripe_pid = pid
             order.original_cart = json.dumps(cart)
             order.save()
+
+            # for each item in bag create a new order line item in admin
             for item_id, item_data in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
+                    product.inventory = product.inventory - item_data
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
                             product=product,
                             quantity=item_data,
                         )
-                        order_line_item.save()
-                        product.inventory = product.inventory - item_data
+                        order_line_item.save()   
                         product.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
