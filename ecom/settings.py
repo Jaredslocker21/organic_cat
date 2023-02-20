3cf4d452db5b004897e4901c28a304645072d607
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-20 20:39:18 +0000

    debug is false

--- ecom/settings.py
+++ ecom/settings.py
@@ -32,7 +32,7 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-jaredslocke-pp5ecommerc-89mg6js85nz.ws-eu8
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['organiccat.herokuapp.com', 'localhost']
 
