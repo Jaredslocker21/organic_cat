9b911121ac2bc8ad8cbaa053946a69c1260d3882
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-20 22:18:43 +0000

    add debug false

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,11 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret! 
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-jaredslocke-pp5ecommerc-89mg6js85nz.ws-eu87.gitpod.io/', 'https://organiccat.herokuapp.com/']
+#CSRF_TRUSTED_ORIGINS = ['https://8000-jaredslocke-pp5ecommerc-89mg6js85nz.ws-eu87.gitpod.io/', 'https://organiccat.herokuapp.com/']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['organiccat.herokuapp.com', 'localhost']
 
