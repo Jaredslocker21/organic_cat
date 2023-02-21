fead63b10ed67a0f28a2e5b417e936534d069940
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-21 14:20:55 +0000

    remove white space

--- cart/contexts.py
+++ cart/contexts.py
@@ -21,16 +21,16 @@ def cart_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-                   
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'cart_items': cart_items,
         'total': total,
