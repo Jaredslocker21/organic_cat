d7b428a25b348403f8f0d6d2974d3492ec2e9016
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-23 23:53:01 +0000

    delete support app and add contact for support

--- ecom/settings.py
+++ ecom/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'profiles',
     'reviews',
-    'support'
+    
     
     # Other
     'storages',
--- ecom/urls.py
+++ ecom/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
-    path('support/', include('support.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
