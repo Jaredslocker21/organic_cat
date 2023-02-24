30f05d1934f8f07fdad4fba5d93f870ab6d2be4e
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-24 00:14:08 +0000

    add django form, views, models and tests.py

---
+++ contact/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import ContactForm
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactForm
+        fields = (
+            'contact_name',
+            'contact_email',
+            'contact_phone_number',
+            'contact_message',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders,
+        remove auto-generated labels
+        set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'contact_name': 'Full Name',
+            'contact_email': 'Email Address',
+            'contact_phone_number': 'Phone Number',
+            'contact_message': 'Message',
+        }
+
+        self.fields['contact_name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+# from .models import ContactForm
 
-# Create your views here.
+from django.contrib import messages
+# from django.conf import settings
+
+
+def contact(request):
+
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Your information was sent successfully. \
+                Please allow up to 7 working days for a response.')
+            return render(request, 'contact/contact_success.html')
+        else:
+            messages.error(request, 'There was an error sending your enquiry. \
+            Please ensure all fields are valid and try again.')
+            return redirect('contact')
+    else:
+        contact_form = ContactForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': contact_form,
+    }
+
+    return render(request, template, context)
