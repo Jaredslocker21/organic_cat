0c2bb2935081cf99e26bbe1652013a65bd516cfa
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-24 00:15:18 +0000

    add test and models too contact app

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .forms import ContactForm
 
-# Create your tests here.
+
+class TestContactForm(TestCase):
+
+    def test_contactform_contact_name_is_required(self):
+        form = ContactForm({'contact_name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('contact_name', form.errors.keys())
+        self.assertEqual(
+            form.errors['contact_name'][0], 'This field is required.')
+
+    def test_contactform_contact_email_is_required(self):
+        form = ContactForm({'contact_email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('contact_email', form.errors.keys())
+        self.assertEqual(
+            form.errors['contact_email'][0], 'This field is required.')
+
+    def test_contactform_contact_message_is_required(self):
+        form = ContactForm({'contact_message': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('contact_message', form.errors.keys())
+        self.assertEqual(
+            form.errors['contact_message'][0], 'This field is required.')
+
+    def test_orderform_contact_phone_number_is_not_required(self):
+        form = ContactForm(
+            {
+                'contact_name': 'A Name',
+                'contact_email': 'email@email.com',
+                'contact_phone_number': '',
+                'contact_message': 'I am a message',
+            })
+        self.assertTrue(form.is_valid)
