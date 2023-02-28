abadab84e72ce79de0ce07529f5d9592f2377a14
Jaredslocker21 <jaredslocker21@gmail.com>
2023-02-28 04:34:35 +0000

    debug is false

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,14 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for productions
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!!
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 CSRF_TRUSTED_ORIGINS = ['https://organiccat.herokuapp.com/']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['organiccat.herokuapp.com', 'localhost']
 
